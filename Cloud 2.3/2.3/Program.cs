b3f4337a087648b191fff799480e3b9c2d02347e
Жирнов Егор <65946665+ZhirnovED@users.noreply.github.com>
2020-05-28 16:57:22 +0400

    Add files via upload

---
+++ Cloud 2.3/2.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+namespace _2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] films = { "Джокер", "Текст", "Богемская рапсодия" };
+            string[] dishes = { "Джокер", "Лед", "Неудержимые" };
+
+            
+            var result = films.Except(dishes);
+            var result1 = films.Intersect(dishes);
+            var result2 = films.Union(dishes);
+
+            Console.WriteLine("Пересечение множеств: ");
+            foreach (string q in result1)
+                Console.WriteLine(q);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Объеденение множеств");
+            foreach (string i in result2)
+                Console.WriteLine(i);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Разность множеств");
+            foreach (string s in result)
+                Console.WriteLine(s);
+        }
+    }
+}
